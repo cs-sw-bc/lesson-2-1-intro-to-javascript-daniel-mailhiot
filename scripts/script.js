2ccaac76eee6abeb30d0563d99ad7761d6648499
daniel-mailhiot <mailhiot2@gmail.com>
2025-10-07 17:48:41 -0700

    JS class 1

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+//External JavaScript
+let c=a+b;
+
+//string
+let name = "Daniel";
+
+//boolean
+let eligbility = false;
+
+//null
+let bug = null; //do not use reserved names for variables
+
+
+let firstName = "Daniel";
+let lastName = "Mailhiot";
+//camel casing - myBirthCountry
+
+//console log - display to console
+
+console.log("My full name is" + firstName + lastName);
+console.log(typeof(eligbility));
+
+//variable naming - cannot start with a number (1name)
+let name1; let _name; let name$;
+
+//constants
+const PI = 3.14;
+const GRAVITY_OF_EARTH = 9.8; //Screaming snake case
+
+let radius = 2;
+
+let area = PI * (radius ** 2);
+
+let modulo = 37%10; //remainder of division operator
+modulo = c%a;
+
+let age = 24;
+eligbility = age>18;
+
+console.log(age);
+console.log(eligbility);
